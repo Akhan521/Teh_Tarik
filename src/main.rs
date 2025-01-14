0797565eaba1434689b4deaa35ca5f4d3d3ee230
Aamir Khan <aamirksfg@gmail.com>
2025-01-14 15:31:39 -0800

    retrieving a commandline arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::env;
+//use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // Check whether a filename was inputted:
+    if args.len() == 1 {
+        println!("Please provide a file to open.");
+        return;
+    } else if args.len() > 2 {
+        println!("Please provide only one file to open.");
+        return;
+    }
+    // Read in the contents of the file.
+    let filename = &args[1];
+    println!("Filename = {filename}");
 }
