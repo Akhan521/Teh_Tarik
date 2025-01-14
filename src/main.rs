e65db1c7f611ab5869430af34ed1d2e5b91f4b5d
abdinava <abdinava26@gmail.com>
2025-01-14 15:47:19 -0800

    added match for file open

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-//use std::fs;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,4 +14,20 @@ fn main() {
     // Read in the contents of the file.
     let filename = &args[1];
     println!("Filename = {filename}");
+
+
+    match fs::read_to_string(filename)
+    {
+        Ok(file_contents) => {
+            println!("Success! File opened :) ");
+            println!("{file_contents}");
+        } 
+
+        Err(e) => {
+            println!("Error! did not open file :( ");
+            println!("{e}");
+        }
+    }
 }
+
+
